0ee558d3fdb6fa13b09ae102f842d137e4587698
Veselin Todorov <veselin@veselin.bg>
2012-03-10 09:10:25 +0200

    Add test for Assertion#header.

--- test/http.js
+++ test/http.js
@@ -8,7 +8,8 @@
  * Context.
  */
 var http = require('../lib/http');
-var chai = require('chai').use(http);
+var chai = require('chai');
+chai.use(http);
 
 /**
  * Support.
@@ -18,7 +19,7 @@ var expect = chai.expect;
 function err(fn, msg) {
   try {
     fn();
-    chai.fail('expected an error');
+    throw new Error('expected an error');
   } catch (err) {
     expect(msg).to.equal(err.message);
   }
@@ -37,5 +38,25 @@ suite('chai-http', function() {
     err(function() {
       expect({}).not.to.have.status(200);
     }, "expected {} to have a property 'statusCode'");
+  });
+
+  test('Assertion#header', function() {
+    var req = {headers: {foo: 'bar'}};
+    var res = {
+      getHeader: function(key) {
+        return 'foo';
+      }
+    };
+
+    expect(req).to.have.header('foo', 'bar');
+    expect(res).to.have.header('bar', 'foo');
+
+    err(function() {
+      expect(req).not.to.have.header('foo', 'bar');
+    }, 'expected header foo to not have value bar');
+
+    err(function() {
+      expect(res).not.to.have.header('bar', 'foo');
+    }, 'expected header bar to not have value foo');
   });
 });
