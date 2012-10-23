8481ad13bbccb7369dc4ddca8593742e33196dfa
Jake Luer <jake.luer@gmail.com>
2012-10-23 11:56:07 -0400

    add support for superagent based testing

---
+++ test/request.js
@@ -0,0 +1,25 @@
+describe('request', function () {
+
+  it('is present on chai', function () {
+    chai.expect(chai).to.respondTo('request');
+  });
+
+  it('can request a functioned "app"', function (done) {
+    var app = function (req, res) {
+      req.headers['x-api-key'].should.equal('testing');
+      res.writeHeader(200, { 'content-type' : 'text/plain' });
+      res.end('hello universe');
+    }
+
+    chai.request(app).get('/')
+      .req(function (req) {
+        req.set('X-API-Key', 'testing')
+      })
+      .res(function (res) {
+        res.should.have.status(200);
+        res.text.should.equal('hello universe');
+        done();
+      });
+  });
+
+});
